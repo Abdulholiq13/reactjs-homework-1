c6c2d0d065ff9ef54ca20183a7947e0b82beb15f
Abdulholiq13 <abdulholiq571@gmail.com>
2024-07-31 09:48:21 +0500

    add services section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/Header';
 import Intro from './components/intro/Intro.jsx';
+import Services from './components/services/Services.jsx';
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
 			<Intro />
+			<Services />
 		</div>
 	);
 }
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,50 @@
+import designIcon from '../../img/intro-design.svg';
+import architectureIcon from '../../img/architecture.svg';
+import planingIcon from '../../img/planing.svg';
+
+import './Services.css';
+
 const Services = () => {
 	return (
 		<section className='services'>
 			<div className='container'>
 				<p className='services__subtitle'>WHAT WE DO</p>
 				<h2 className='services__title'>Our Service</h2>
+
+				<div className='services__list'>
+					<div className='services__item'>
+						<img src={designIcon} alt='' width={40} height={40} />
+						<span className='services__item-texts'>
+							<h3 className='services__item-title'>Interior Design</h3>
+							<p className='services__item-text'>
+								Lorem ipsum dolor amet consectetur adipiscing elit sed eiusmod tempor incididunt ut
+								labore.
+							</p>
+						</span>
+					</div>
+
+					<div className='services__item item-brown'>
+						<img src={architectureIcon} alt='' width={40} height={40} />
+						<span className='services__item-texts'>
+							<h3 className='services__item-title'>Architecture</h3>
+							<p className='services__item-text item-brown-text'>
+								Lorem ipsum dolor amet consectetur adipiscing elit sed eiusmod tempor incididunt ut
+								labore.
+							</p>
+						</span>
+					</div>
+
+					<div className='services__item'>
+						<img src={planingIcon} alt='' width={40} height={40} />
+						<span className='services__item-texts'>
+							<h3 className='services__item-title'>Planning</h3>
+							<p className='services__item-text'>
+								Lorem ipsum dolor amet consectetur adipiscing elit sed eiusmod tempor incididunt ut
+								labore.
+							</p>
+						</span>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
